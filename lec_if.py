7f1be6a2a0a1ee63c386bd126c04264551147d93
Semakina1 <semakinamarial@mail.ru>
2025-10-17 16:37:58 +0000

    очень круто

---
+++ lec_if.py
@@ -0,0 +1,16 @@
+''' - Документ строка
+
+# if - условие
+
+'''
+
+if 1:
+    print('hello 1')
+
+a = 3
+if a > 1:
+    print(f'hello {a}')
+b = 5 
+if b == 5: #Операция сранения
+    print(f'hello {b}')
+
