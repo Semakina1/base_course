e833dd390d8b27e06cc671a5bb17e5cac4537da7
Semakina1 <semakinamarial@mail.ru>
2025-11-28 14:58:12 +0000

    uyht

---
+++ __pycache__/lec_math.py
@@ -0,0 +1,7 @@
+import math
+ 
+a = math.sin(3 * math.pi / 5)
+print(a)
+
+b = math.log10(42)
+print(b)
---
+++ lec_import.py
@@ -0,0 +1,10 @@
+import lec_my_module
+
+print(lec_my_module.a) #оператор представляющий доступ к отрибутам модуля
+
+b = lec_my_module.b * 3
+
+print(b)
+
+print(lec_my_module.c[2])
+
