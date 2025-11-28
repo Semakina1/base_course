25aabe849d0cb812ba18eeaad8ea881506bc058b
Semakina1 <semakinamarial@mail.ru>
2025-11-28 16:45:23 +0000

    крутойчувак

---
+++ lec_data_types.py
@@ -0,0 +1,28 @@
+x = 3
+y = 4
+
+z = complex(y, x)
+print(z)
+
+w = complex(y, x)
+print(z + w)
+
+s = 'hello'
+print(s[0])
+
+t = (1, 4, 9)
+print(t)
+print(t[0])
+'t[0] = 3'
+
+k = [1, 4, 9]
+k[0] = 3
+print(k)
+
+d = {'key_1': 4,2: 'red', 'str': 'hello'}
+print(d['key_1'])
+print(d[2])
+print(d['str'])
+
+d['str'] = 'good'
+print(d)
---
+++ lec_документирование.py
@@ -0,0 +1,6 @@
+def crutoi_chuvak(a=1, b=1, c=1):
+    a = 'pofig'
+    b = 'pofig'
+     print('ты классный')
+
+сг
